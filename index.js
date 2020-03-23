e34e6d0b407f8621b5e14626f84657789398f87a
Luke Luo <guozhong.luo@ext.mercedes-benz.io>
2020-03-23 10:26:59 +0800

    update

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const fs = require('fs')
 const path = require('path')
 
-const SOURCE_DIRECTORY = './js_demo'
+const SOURCE_DIRECTORY = './src_demo'
 const FILENAME_REGEXP = new RegExp(/\.(html|vue|js)$/, 'g')
-const I18N_REGEXP = new RegExp(/(\$t|\$i18n.t)\(\S*"\)/, 'gm')
+const I18N_REGEXP = new RegExp(/(\$t|\$i18n.t)\(\S*['"]\)/, 'gm')
 
 // get all files by folder
 function getAllFiles(dirPath, arrayOfFiles) {
